eb16f196a37b4c4d2fb33ee985b0294c6c8c8b5f
Christer Enfors <christer.enfors@gmail.com>
2018-08-15 14:31:15 +0200

    Add more and better SQLAlchemy examples

--- azurabot/user.py
+++ azurabot/user.py
@@ -3,7 +3,7 @@
 """
 The User class for storing user data.
 """
-
+from sqlalchemy.orm.exc import NoResultFound
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime
 
@@ -18,15 +18,18 @@ class User(Base):
     __tablename__ = "users"
     user_id = Column(Integer, primary_key=True)
     name = Column(String(20), index=True)
+    comment = Column(String(80))
     created_on = Column(DateTime(), default=datetime.now)
     updated_on = Column(DateTime, default=datetime.now,
                         onupdate=datetime.now())
 
     def __init__(self, name):
         self.name = name
+        self.comment = ""
 
     def __str__(self):
-        return("User %s (%s)" % (self.name, self.user_id))
+        return("User %s[%d]: \"%s\"" % (self.name, self.user_id,
+                                        self.comment))
 
 
 if __name__ == "__main__":
@@ -42,7 +45,40 @@ if __name__ == "__main__":
     session.commit()
     print(enfors)
 
-    print("Loading user...")
-    query = session.query(User).filter(User.name.like("Enfors"))
+    # How to load many rows at once
+    # =============================
+    #
+    # The "query" returned is an iterable containing all the
+    # matching users.
+    print("Loading non-existant user...")
+    query = session.query(User).filter(User.name == "non-existant")
     for user in query:
         print(user)
+
+    # How to load ONE row
+    # ===================
+    #
+    # In this case, we don't get a "query" as a return value,
+    # because we're using one(). That gives us ONE object in
+    # return, of the correct type, so no need to iterate over
+    # a query object.
+    print("Loading one user which does exist...")
+    try:
+        user = session.query(User).filter(User.name == "Enfors").one()
+        print(user)
+
+    except NoResultFound:
+        print("[that user doesn't exist]")
+
+    # How to update and save
+    # ======================
+    user.comment = "www.OperationGYST.com"
+    session.commit()
+
+    print("Reloading user as user2...")
+    try:
+        user2 = session.query(User).filter(User.name == "Enfors").one()
+        print(user2)
+
+    except NoResultFound:
+        print("[that user doesn't exist]")
