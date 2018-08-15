93a851dc92acab7d2b17d29d64455f72d96eead5
Christer Enfors <christer.enfors@gmail.com>
2018-08-15 13:14:56 +0200

    Add POC code to user.py
    Adds some testing code to user.py, which tests the saving and loading
    of the user data. This is a good template for how to load and save
    things in SQLAlchemy.

--- azurabot/user.py
+++ azurabot/user.py
@@ -4,11 +4,17 @@
 The User class for storing user data.
 """
 
-import datetime
+from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-class User:
+Base = declarative_base()
+
+
+class User(Base):
     __tablename__ = "users"
     user_id = Column(Integer, primary_key=True)
     name = Column(String(20), index=True)
@@ -20,4 +26,23 @@ class User:
         self.name = name
 
     def __str__(self):
-        return("User %d (%d)" % (self.name, self.user_id))
+        return("User %s (%s)" % (self.name, self.user_id))
+
+
+if __name__ == "__main__":
+    engine = create_engine("sqlite:///:memory:", echo=False)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    Base.metadata.create_all(engine)
+
+    print("Creating user...")
+    enfors = User("Enfors")
+    session.add(enfors)
+    session.commit()
+    print(enfors)
+
+    print("Loading user...")
+    query = session.query(User).filter(User.name.like("Enfors"))
+    for user in query:
+        print(user)
