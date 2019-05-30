fa4d37a31ad90704ca606ed8f7a12f62e540d297
Christer Enfors <christer.enfors@gmail.com>
2019-05-30 19:42:09 +0200

    Fix slack_test3 so that it works

--- exp/slack_test3.py
+++ exp/slack_test3.py
@@ -1,4 +1,5 @@
 import os
+import pprint
 
 import slack
 
@@ -7,28 +8,33 @@ def read_secret(section: str, file_name: str):
     with open(f"../secret/{section}/{file_name}") as f:
         return f.readline().strip()
 
+pp = pprint.PrettyPrinter(indent=4)
 
-slack_token = read_secret("slack", "bot_user_oauth_access_token")
-
-# rtmclient = slack.RTMClient(token=slack_token)
-rtmclient = slack.RTMClient(token=slack_token)
 
 @slack.RTMClient.run_on(event='message')
 def say_hello(**payload):
     print("input")
     data = payload['data']
+    pp.pprint(data)
     web_client = payload['web_client']
     rtm_client = payload['rtm_client']
-    if 'Hello' in data['text']:
+    if "text" in data and "Hello" in data['text']:
         channel_id = data['channel']
         thread_ts = data['ts']
         user = data['user']
 
-        webclient.chat_postMessage(
+        web_client.chat_postMessage(
             channel=channel_id,
             text="Hi <@{}>!".format(user),
             thread_ts=thread_ts
         )
 
-print("Running.")
+slack_token = read_secret("slack", "bot_user_oauth_access_token")
+
+# rtmclient = slack.RTMClient(token=slack_token)
+rtmclient = slack.RTMClient(token=slack_token)
+print("Token:", slack_token)
+
+print("Starting.")
 rtmclient.start()
+print("Exiting.")
